541d19b362fcc6bd3178b0c6b2eab61e5b74660d
Tai Vo <tai.vo@infodation.vn>
2022-04-25 23:53:05 +0700

    add paging and table

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -1,14 +1,73 @@
 import React from "react";
+import Pagination from "react-bootstrap/Pagination";
+import Table from "react-bootstrap/Table";
+import "./table.scss";
 
 const TableTop = () => {
   return (
     <div className="table-top">
-      <div className="table-top_result"></div>
+      <div className="table-top_result">101 Kết quả</div>
+      <Pagination>
+        <Pagination.First />
+        <Pagination.Prev />
+        <Pagination.Item>{1}</Pagination.Item>
+        <Pagination.Ellipsis />
+
+        <Pagination.Item>{10}</Pagination.Item>
+        <Pagination.Item>{11}</Pagination.Item>
+        <Pagination.Item active>{12}</Pagination.Item>
+        <Pagination.Item>{13}</Pagination.Item>
+        <Pagination.Item disabled>{14}</Pagination.Item>
+
+        <Pagination.Ellipsis />
+        <Pagination.Item>{20}</Pagination.Item>
+        <Pagination.Next />
+        <Pagination.Last />
+      </Pagination>
     </div>
   );
 };
+const TableData = () => {
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan="2">Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+};
+
 const BlockTable = () => {
-  return <div className="block-table"></div>;
+  return (
+    <div className="block-table">
+      <TableTop />
+      <TableData />
+    </div>
+  );
 };
 
 export default BlockTable;
