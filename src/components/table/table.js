d43ac107a8f22b0b01d6a6c0a5b4c302f5f0a694
Tai Vo <tai.vo@infodation.vn>
2022-04-26 14:32:05 +0700

    done page master

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -1,63 +1,40 @@
 import React from "react";
 import Pagination from "react-bootstrap/Pagination";
-import Table from "react-bootstrap/Table";
+import Dropdown from "react-bootstrap/Dropdown";
 import "./table.scss";
+import TableData from "./tableData";
 
 const TableTop = () => {
   return (
     <div className="table-top">
       <div className="table-top_result">101 Kết quả</div>
-      <Pagination>
-        <Pagination.First />
-        <Pagination.Prev />
-        <Pagination.Item>{1}</Pagination.Item>
-        <Pagination.Ellipsis />
+      <div className="table-top_paging">
+        <Pagination>
+          <Pagination.Prev disabled />
+          <Pagination.Item>{1}</Pagination.Item>
+          <Pagination.Item active>{2}</Pagination.Item>
+          <Pagination.Ellipsis />
 
-        <Pagination.Item>{10}</Pagination.Item>
-        <Pagination.Item>{11}</Pagination.Item>
-        <Pagination.Item active>{12}</Pagination.Item>
-        <Pagination.Item>{13}</Pagination.Item>
-        <Pagination.Item disabled>{14}</Pagination.Item>
+          <Pagination.Item>{10}</Pagination.Item>
 
-        <Pagination.Ellipsis />
-        <Pagination.Item>{20}</Pagination.Item>
-        <Pagination.Next />
-        <Pagination.Last />
-      </Pagination>
+          <Pagination.Next />
+        </Pagination>
+        <div className="table-top_paging-page">
+          <Dropdown>
+            <Dropdown.Toggle variant="Secondary" id="8">
+              8
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">10</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">20</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">50</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          <span>Kết quả</span>
+        </div>
+      </div>
     </div>
-  );
-};
-const TableData = () => {
-  return (
-    <Table>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan="2">Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </Table>
   );
 };
 
---
+++ src/components/table/tableData.js
@@ -0,0 +1,254 @@
+import React from "react";
+import Table from "react-bootstrap/Table";
+import { v4 as uuidv4 } from "uuid";
+import "./table.scss";
+
+const rowTitle = [
+  "Mã Đấu thầu",
+  "Người phụ trách",
+  "Mã HSMT",
+  "Gói thầu",
+  "Phương thức đấu thầu",
+  "Thời gian đóng thầu",
+  "",
+  "",
+];
+
+const datasColumn = [
+  {
+    maDauThau: {
+      label: "DT-001/2021",
+      type: "link",
+      subLabel: "Mã đấu thầu",
+    },
+    nguoiPhuTrach: {
+      label: "Nguyễn Diệu Linh",
+      type: "text",
+      subLabel: "linhnd@fpt.com.vn",
+    },
+    maHSMT: {
+      label: "HSMT_001/2022",
+      type: "link",
+      subLabel: "Mã HSMT",
+    },
+    goiThau: {
+      label: "Gói thầu mua sắm",
+      type: "text",
+      subLabel: "Tên gói thầu",
+    },
+    phuongThucDauThau: {
+      label: "Đấu thầu 1 GĐ",
+      type: "text",
+      subLabel: "Phương thức đấu thầu",
+    },
+    phuongThucDongThau: {
+      label: "15/12/2022",
+      type: "text",
+      subLabel: "Phương thức đóng thầu",
+    },
+    trangThai: {
+      label: "Nộp hồ sơ",
+      type: "status",
+      subLabel: "inprogess",
+    },
+    hanhDong: {
+      label: "icon",
+      type: "icon",
+      subLabel: "",
+    },
+  },
+  {
+    maDauThau: {
+      label: "DT-001/2021",
+      type: "link",
+      subLabel: "Mã đấu thầu",
+    },
+    nguoiPhuTrach: {
+      label: "Nguyễn Diệu Linh",
+      type: "text",
+      subLabel: "linhnd@fpt.com.vn",
+    },
+    maHSMT: {
+      label: "HSMT_001/2022",
+      type: "link",
+      subLabel: "Mã HSMT",
+    },
+    goiThau: {
+      label: "Gói thầu mua sắm",
+      type: "text",
+      subLabel: "Tên gói thầu",
+    },
+    phuongThucDauThau: {
+      label: "Đấu thầu 1 GĐ",
+      type: "text",
+      subLabel: "Phương thức đấu thầu",
+    },
+    phuongThucDongThau: {
+      label: "15/12/2022",
+      type: "text",
+      subLabel: "Phương thức đóng thầu",
+    },
+    trangThai: {
+      label: "Nộp hồ sơ",
+      type: "status",
+      subLabel: "close",
+    },
+    hanhDong: {
+      label: "icon",
+      type: "icon",
+      subLabel: "",
+    },
+  },
+  {
+    maDauThau: {
+      label: "DT-001/2021",
+      type: "link",
+      subLabel: "Mã đấu thầu",
+    },
+    nguoiPhuTrach: {
+      label: "Nguyễn Diệu Linh",
+      type: "text",
+      subLabel: "linhnd@fpt.com.vn",
+    },
+    maHSMT: {
+      label: "HSMT_001/2022",
+      type: "link",
+      subLabel: "Mã HSMT",
+    },
+    goiThau: {
+      label: "Gói thầu mua sắm",
+      type: "text",
+      subLabel: "Tên gói thầu",
+    },
+    phuongThucDauThau: {
+      label: "Đấu thầu 1 GĐ",
+      type: "text",
+      subLabel: "Phương thức đấu thầu",
+    },
+    phuongThucDongThau: {
+      label: "15/12/2022",
+      type: "text",
+      subLabel: "Phương thức đóng thầu",
+    },
+    trangThai: {
+      label: "Nộp hồ sơ",
+      type: "status",
+      subLabel: "open",
+    },
+    hanhDong: {
+      label: "icon",
+      type: "icon",
+      subLabel: "",
+    },
+  },
+  {
+    maDauThau: {
+      label: "DT-001/2021",
+      type: "link",
+      subLabel: "Mã đấu thầu",
+    },
+    nguoiPhuTrach: {
+      label: "Nguyễn Diệu Linh",
+      type: "text",
+      subLabel: "linhnd@fpt.com.vn",
+    },
+    maHSMT: {
+      label: "HSMT_001/2022",
+      type: "link",
+      subLabel: "Mã HSMT",
+    },
+    goiThau: {
+      label: "Gói thầu mua sắm",
+      type: "text",
+      subLabel: "Tên gói thầu",
+    },
+    phuongThucDauThau: {
+      label: "Đấu thầu 1 GĐ",
+      type: "text",
+      subLabel: "Phương thức đấu thầu",
+    },
+    phuongThucDongThau: {
+      label: "15/12/2022",
+      type: "text",
+      subLabel: "Phương thức đóng thầu",
+    },
+    trangThai: {
+      label: "Nộp hồ sơ",
+      type: "status",
+      subLabel: "success",
+    },
+    hanhDong: {
+      label: "icon",
+      type: "icon",
+      subLabel: "",
+    },
+  },
+];
+
+const switchType = (type, status = null) => {
+  if (type === "link") return "label-link";
+  if (type === "text") return "label-text";
+  if (type === "icon") return "label-icon";
+  if (type === "status") {
+    if (status === "inprogess") return "label-status label-status-inprogess";
+    if (status === "open") return "label-status label-status-open";
+    if (status === "success") return "label-status label-status-success";
+    if (status === "close") return "label-status label-status-close";
+  }
+  return "";
+};
+
+const TableData = () => {
+  return (
+    <Table>
+      <thead>
+        <tr key={uuidv4()}>
+          {rowTitle.map((item) => (
+            <th key={uuidv4()}>{item}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {datasColumn.map((item) => {
+          return (
+            <tr key={uuidv4()}>
+              {Object.entries(item).map(([key, val]) => {
+                return (
+                  <td key={uuidv4()}>
+                    <div className={switchType(val.type, val.subLabel)}>
+                      {val.label === "icon" ? (
+                        <svg
+                          width="16"
+                          height="4"
+                          viewBox="0 0 16 4"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            clip-rule="evenodd"
+                            d="M2 0C3.10457 0 4 0.89543 4 2C4 3.10457 3.10457 4 2 4C0.89543 4 0 3.10457 0 2C0 0.89543 0.89543 0 2 0ZM10 2C10 0.89543 9.10457 0 8 0C6.89543 0 6 0.89543 6 2C6 3.10457 6.89543 4 8 4C9.10457 4 10 3.10457 10 2ZM14 0C15.1046 0 16 0.89543 16 2C16 3.10457 15.1046 4 14 4C12.8954 4 12 3.10457 12 2C12 0.89543 12.8954 0 14 0Z"
+                            fill="black"
+                          />
+                        </svg>
+                      ) : (
+                        val.label
+                      )}
+                    </div>
+                    {val.subLabel && val.type !== "status" ? (
+                      <div className="label-sub-text">{val.subLabel}</div>
+                    ) : (
+                      ""
+                    )}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </Table>
+  );
+};
+
+export default TableData;
